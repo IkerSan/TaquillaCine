0168e99259e673643dd8bb87f58a2da9c27d841c
IkerSan <cosasquequieres@gmail.com>
2025-12-17 20:59:53 +0100

    Clase cine implementada

--- taquillas/src/main/java/cuatrovientos/dam/psp/sincronizacion/taquillas/Cine.java
+++ taquillas/src/main/java/cuatrovientos/dam/psp/sincronizacion/taquillas/Cine.java
@@ -4,4 +4,38 @@ import java.util.concurrent.Semaphore;
 
 public class Cine {
     
+    // Semáforo para controlar los asientos disponibles (200)
+    private Semaphore asientos;
+    private int entradasVendidas = 0;
+    private int clientesRechazados = 0; 
+
+    public Cine(int numeroAsientos) {
+        this.asientos = new Semaphore(numeroAsientos);
+    }
+
+    // Intenta comprar entrada
+    public boolean comprarEntrada() {
+        if (asientos.tryAcquire()) {
+            synchronized (this) {
+                entradasVendidas++;
+            }
+            return true;
+        } else {
+            // Rechaza si no hay asientos disponibles
+            incrementarRechazados();
+            return false;
+        }
+    }
+
+    public synchronized void incrementarRechazados() {
+        clientesRechazados++;
+    }
+
+    public int getEntradasVendidas() {
+        return entradasVendidas;
+    }
+
+    public int getClientesRechazados() {
+        return clientesRechazados;
+    }
 }
